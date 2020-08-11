96725872a4ee40a34421bb7612a2e6eeae3089e6
DESKTOP-56F9530\ucadena07 <ulises@netkinetix.com>
2020-08-11 06:18:37 -0500

    Add project files.

---
+++ BulkyBooks/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BulkyBooks.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
